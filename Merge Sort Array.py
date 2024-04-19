68b24781ec1567d0ede4bf1ea67e5ca92182dae8
M.ngugi <mngugi7@gmail.com>
2024-04-19 14:13:29 +0300

    Update Merge Sort Array.py

--- Merge Sort Array.py
+++ Merge Sort Array.py
@@ -1,6 +1,28 @@
 info = """
-You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
-and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,and
+two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside
+the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements
+denote the elements that should be merged, and the last n elements are set to 0 and should
+be ignored. nums2 has a length of n.
 
 """
 print(info)
+
+def bubble_sort(arr):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+            # Traverse the array from 0 to n - i - 1
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+# Test the bubble_sort function
+arr = [31, 41, 59, 26, 41, 58]
+bubble_sort(arr)
+print("Sorted array:", arr)
