4d0630374b321e39fc20f7a7afcccec7354173c6
M.ngugi <mngugi7@gmail.com>
2024-04-19 10:50:25 +0300

    Create Merge Sort Array.py

---
+++ Merge Sort Array.py
@@ -0,0 +1,6 @@
+info = """
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+"""
+print(info)
