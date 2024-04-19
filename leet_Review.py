1c9a7475cd64c6ab50a6513233a43e17dd6187a2
M.ngugi <mngugi7@gmail.com>
2024-04-19 19:00:57 +0300

    Update leet_Review.py

--- leet_Review.py
+++ leet_Review.py
@@ -5,8 +5,18 @@ Author : Mwangi
 Purpose : Leet Recap
 programming starts at function modules
 """
+import os
+import keyword 
 
 def building_house():
     house = " Foundation ", "Walls", "Roof"
     return house
 print("House Component", building_house()) 
+
+# program to print softkwlist using functions
+
+def keywords():
+    key_words = keyword.softkwlist
+    return key_words
+print(keywords())
+
