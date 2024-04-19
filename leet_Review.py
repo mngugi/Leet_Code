2e1db6c8cbf04ae4193bdee205b69913b3081ded
M.ngugi <mngugi7@gmail.com>
2024-04-19 18:52:02 +0300

    Create leet_Review.py

---
+++ leet_Review.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : Leet Recap
+programming starts at function modules
+"""
+
+def building_house():
+    house = " Foundation ", "Walls", "Roof"
+    return house
+print("House Component", building_house()) 
