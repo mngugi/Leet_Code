f6bde29190dfa637b78b6f6e15a69d1de8709bbd
P.M.Ngugi <mngugi7@gmail.com>
2025-06-16 19:41:44 +0300

    Update maximumDifference.cpp

--- maximumDifference.cpp
+++ maximumDifference.cpp
@@ -1,3 +1,38 @@
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# Code
+```cpp []
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int minVal = nums[0];
+        int maxDiff = -1;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > minVal) {
+                maxDiff = max(maxDiff, nums[i] - minVal);
+            } else {
+                minVal = nums[i]; 
+            }
+        }
+       return maxDiff; 
+    }
+};
+```
+
+**Solution**
+
 #include <vector>
 #include <algorithm> // for std::max
 using namespace std;
